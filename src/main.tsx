16c3be632f2f6aabccb98a4ace5a65daa8b4678e
leonardochavezcornejo <leonardo.chavez.cornejo@gmail.com>
2025-05-05 11:32:18 -0500

    REACT

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import MainPage from './pages/MainPage'
 
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+//import de boostrap
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  
-    </StrictMode>,
+  <MainPage/>
+  </StrictMode>,
 )
